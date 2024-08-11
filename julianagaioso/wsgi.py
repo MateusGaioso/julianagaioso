3304c3e283f4232b9ddb004b58c33a24fbc654e7
Mateus Gaioso <mateusgaioso@gmail.com>
2024-08-11 15:27:25 -0400

    wsgi config

--- julianagaioso/wsgi.py
+++ julianagaioso/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Adicione o caminho do projeto ao sys.path
+sys.path.append('/home/ubuntu/juliana_gaioso/julianagaioso')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "julianagaioso.settings")
 
